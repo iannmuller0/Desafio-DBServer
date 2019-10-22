4e37ed88d5a1cf726a6918fb589846d11b55c628
iann <iann@vakinha.com>
2019-10-22 19:05:20 -0300

    adding blank line in the end of file

--- src/pages/restaurants/restaurants.tsx
+++ src/pages/restaurants/restaurants.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import RestaurantList from '../../mockData/RestaurantList';
+import ShowRestaurantList from '../../components/ShowRestaurantsList/ShowRestaurantList';
 import Navbar from '../../components/Navbar/Navbar';
-import { map } from 'lodash';
 import { Container } from 'react-grid-system';
 
 const Restaunt = () => {
   return (
     <Container>
       <Navbar />
-      <ul>
-        { 
-          map(RestaurantList, (restaunt: any) => (
-            <li key={restaunt.id}>{restaunt.name} - {restaunt.address}</li>
-          ))
-        }
-      </ul>
+        <ShowRestaurantList />
     </Container>
   );
 };
