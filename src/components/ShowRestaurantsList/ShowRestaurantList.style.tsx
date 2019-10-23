7fb200d13a487cb4cee4b9e144ce12ec2fe249bd
iann <iann@vakinha.com>
2019-10-23 01:46:08 -0300

    add files missing in other branch

---
+++ src/components/ShowRestaurantsList/ShowRestaurantList.style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const RestaurantsItems = styled.ul`
+  list-style-type: none;
+  flex-direction: column;
+  display: flex;
+`;
+
+export const RestaurantsItem = styled.li`
+  margin-bottom: 10px
+`;
